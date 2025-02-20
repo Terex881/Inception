bccd2ca06589f38ac177a1ed34dffb8b97d2c6d9
Salah Demnati <sdemnati@e3r3p17.1337.ma>
2025-02-20 19:02:09 +0100

    trying to connect wordress and mariadb

--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -1,61 +1,20 @@
 #!/bin/bash
 
-# Remove existing WordPress files (optional, but careful)
-rm -rf /var/www/wordpress/*
-
-# Ensure the WordPress directory exists
-mkdir -p /var/www/wordpress
-cd /var/www/wordpress || exit
-
-# Download WP-CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
 chmod +x wp-cli.phar
+
 mv wp-cli.phar /usr/local/bin/wp
 
-# Download WordPress
-wp core download --allow-root
+WP_PATH=/var/www/wordpress
 
-# Create wp-config.php
-wp config create --allow-root --dbname=hello_db --dbuser=terex --dbpass=zxcvbnm --dbhost=mariadb
+wp core download --path=$WP_PATH --allow-root
 
-# Install WordPress
-wp core install --allow-root --url=sdemnati.42.fr --title="WELCOME" \
-    --admin_user=terex --admin_password=zxcvbnm --admin_email=1337@gmail.com
+mv $WP_PATH/wp-config-sample.php $WP_PATH/wp-config.php
 
-# Create a user
-wp user create --allow-root terex 1337@gmail.com --user_pass=zxcvbnm --role=administrator
+wp user create --allow-root terex 1337@gmail.com --user_pass=zxcvbnm  --path=$WP_PATH
+wp config create --allow-root --dbname=hello_db --dbuser=terex --dbpass=zxcvbnm  --path=$WP_PATH
 
-# Change ownership of WordPress files
-chown -R www-data:www-data /var/www/wordpress
-chmod -R 755 /var/www/wordpress
+wp core install --allow-root --url=sdemnati.42.fr --title="WELCOME" --admin_user=terex --admin_password=zxcvbnm --admin_email=1337@gmail.com  --path=$WP_PATH
 
-# Keep the container running
-php-fpm -F
-
-# # rm -rf /var/www/wordpress/*
-
-# mkdir -p /var/www/wordpress
-
-
-# cd /var/www/wordpress
-
-# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-# chmod +x wp-cli.phar
-
-# mv wp-cli.phar /usr/local/bin/wp
-
-# # chmod -R 777 
-
-# wp core download --allow-root
-# # wp core download
-
-# wp user create --allow-root terex 1337@gmail.com --user_pass=zxcvbnm
-# wp config create --allow-root --dbname=hello_db --dbuser=terex --dbpass=zxcvbnm
-
-# wp core install --allow-root --url=sdemnati.42.fr --title="WELCOME" --admin_user=terex --admin_password=zxcvbnm --admin_email=1337@gmail.com
-# chown -R www-data:www-data /var/www/wordpress
-
-# # chmod -R 755 /var/www/wordpress
-
-# php-fpm -F
+/usr/sbin/php-fpm7.4 -F
