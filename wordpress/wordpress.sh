f71974ed1d7557d2c8d60d7cf33ab528cfd72b31
Terex <salaminty123@gmail.com>
2025-02-24 20:40:18 +0000

    tring to fix volumes

--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+# Download WP-CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
-
 mv wp-cli.phar /usr/local/bin/wp
 
-WP_PATH=/var/www/wordpress
+mkdir -p /var/www/wordpress
 
-wp core download --path=$WP_PATH --allow-root
+cd /var/www/wordpress
 
-mv $WP_PATH/wp-config-sample.php $WP_PATH/wp-config.php
+# Download WordPress core
+wp core download --allow-root
 
-wp user create --allow-root terex 1337@gmail.com --user_pass=zxcvbnm  --path=$WP_PATH
-wp config create --allow-root --dbname=hello_db --dbuser=terex --dbpass=zxcvbnm  --path=$WP_PATH
+# Configure database connection before user creation
+mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
-wp core install --allow-root --url=sdemnati.42.fr --title="WELCOME" --admin_user=terex --admin_password=zxcvbnm --admin_email=1337@gmail.com  --path=$WP_PATH
+wp config create --allow-root --dbname=hello_db --dbuser=terex --dbpass=zxcvbnm
 
-/usr/sbin/php-fpm7.4 -F
+# Install WordPress
+wp core install --allow-root \
+  --url=sdemnati.42.fr \
+  --title="WELCOME" \
+  --admin_user=terex \
+  --admin_password=zxcvbnm \
+  --admin_email=1337@gmail.com \
+  --path=/var/www/wordpress
+
+
+sed -i 's|^listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
+
+# Start PHP-FPM in the foreground
+exec php-fpm7.4 -F
