5534bb16542ca81204f98ac4bbdb8f6a9c6556db
Terex881 <salaminty123@gmail.com>
2025-03-16 18:04:49 +0000

    fix ftp

--- bonus/ftp/ftp.sh
+++ bonus/ftp/ftp.sh
@@ -3,23 +3,23 @@
 service vsftpd start
 
 useradd -m "$USERFTP"
-echo "$USERFTP:$PASSFTP" | chpasswd
+echo  "$USERFTP:$PASSFTP" | chpasswd
 
 mkdir -p /home/$USERFTP/ftp
 chown -R "$USERFTP:$USERFTP" /home/$USERFTP/ftp
 
 echo "$USERFTP" >> /etc/vsftpd.userlist
-   
-echo "pasv_enable=YES
-pasv_address=0.0.0.0
+
+echo "anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+user_sub_token=$USERFTP
+local_root=/home/$USERFTP/ftp
 pasv_min_port=50000
 pasv_max_port=50001
-listen=YES         
-anonymous_enable=NO
-local_enable=YES   
-write_enable=YES
-userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
-
+userlist_enable=YES
+userlist_file=/etc/vsftpd.userlist
+userlist_deny=NO" >> /etc/vsftpd.conf
 
 service vsftpd stop
 
